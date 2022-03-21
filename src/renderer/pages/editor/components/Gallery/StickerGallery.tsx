c57843a7c5ea9d93c3493a24e5b5cc2a4c33233c
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-03-21 22:44:23 +0700

    Add : StickerPreview Component

--- src/renderer/pages/editor/components/Gallery/StickerGallery.tsx
+++ src/renderer/pages/editor/components/Gallery/StickerGallery.tsx
@@ -1,22 +1,12 @@
 import balloon from '../../../../../../public/stickers/balloon.png';
 import crocodile from '../../../../../../public/stickers/crocodile.png';
 import hny from '../../../../../../public/stickers/happy-new-year.png';
-import { useEditorContext } from '../../context';
-import { useState } from 'react';
+import StickerPreview from './components/StickerPreview';
 
 const mock_sticker = [
   {
     name: 'Party',
-    stickerSets: [
-      balloon,
-      balloon,
-      balloon,
-      balloon,
-      balloon,
-      balloon,
-      balloon,
-      balloon,
-    ],
+    stickerSets: [balloon],
   },
   {
     name: 'Animals',
@@ -28,8 +18,6 @@ const mock_sticker = [
   },
 ];
 function StickerGallery() {
-  const { setSelectSticker } = useEditorContext();
-
   return (
     <div className="p-4 h-2">
       <h1 className="text-4xl font-bold">Stickers</h1>
@@ -37,22 +25,9 @@ function StickerGallery() {
         {mock_sticker.map(({ name, stickerSets }) => (
           <div className="grid grid-cols-2 gap-4 mb-4" key={name}>
             <h2 className="col-span-2 text-xl font-bold">{name}</h2>
-            {stickerSets.map((sticker) => {
-              const [isDragging, setIsDragging] = useState<boolean>(false);
-              return (
-                <div className="p-4 bg-gray-100 rounded-lg" key={sticker}>
-                  <img
-                    src={sticker}
-                    className={isDragging ? 'opacity-0' : 'opacity-100'}
-                    onDragStart={() => {
-                      setIsDragging(true);
-                      setSelectSticker(sticker);
-                    }}
-                    onDragEnd={() => setIsDragging(false)}
-                  />
-                </div>
-              );
-            })}
+            {stickerSets.map((sticker) => (
+              <StickerPreview sticker={sticker} key={sticker} />
+            ))}
           </div>
         ))}
       </div>
---
+++ src/renderer/pages/editor/components/Gallery/components/StickerPreview.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useEditorContext } from '../../../context';
+
+function StickerPreview({ sticker }: { sticker: string }) {
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const { setSelectSticker } = useEditorContext();
+
+  return (
+    <div className="p-4 bg-gray-100 rounded-lg">
+      <img
+        src={sticker}
+        className={isDragging ? 'opacity-0' : 'opacity-100'}
+        onDragStart={() => {
+          setIsDragging(true);
+          setSelectSticker(sticker);
+        }}
+        onDragEnd={() => setIsDragging(false)}
+      />
+    </div>
+  );
+}
+
+export default StickerPreview;
