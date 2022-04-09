99c26788e9012e0a9495e54da33285ff1941e9c8
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-04-09 22:53:16 +0700

    Add : Drag and Drop import

--- src/renderer/pages/preference/Stickers/components/InputSticker.tsx
+++ src/renderer/pages/preference/Stickers/components/InputSticker.tsx
@@ -1,3 +1,4 @@
+import { DragEvent } from 'react';
 interface StickerInterface {
   src: string;
 }
@@ -13,7 +14,25 @@ function InputSticker({
         .then((stickers: StickerInterface[]) => onImport(stickers));
     }
   };
-
+  const handleOnDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const filePaths = Array.from(e.dataTransfer.files).map((file) => file.path);
+    const isImage = /image\//g.test(e.dataTransfer.files[0].type);
+    if (e.dataTransfer.files && isImage)
+      window.electron.stickers
+        .import(filePaths)
+        .then((stickers: StickerInterface[]) => onImport(stickers));
+  };
+  return (
+    <div
+      className="w-full h-36 border-2 flex justify-center items-center"
+      onDragOver={(e) => e.preventDefault()}
+      onDrop={handleOnDrop}
+      onClick={() => window.electron.files.choose('file').then(handleSelected)}
+    >
+      <h1>Click or Drop Sticker here</h1>
+    </div>
+  );
   return (
     <div
       className="w-full h-36 border-2 flex justify-center items-center"
