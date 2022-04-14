c7893466fd4a741b545fbb7075beb95891aa5765
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-04-14 14:51:30 +0700

    Add : upload to cloud storage on add photo to upload dir

--- src/main/ipcHandle/upload/index.ts
+++ src/main/ipcHandle/upload/index.ts
@@ -3,52 +3,10 @@ import crypto from 'crypto';
 import path from 'path';
 import fs from 'fs';
 import { PhotoInterface } from 'renderer/utils/interface';
-import admin, { storage } from 'firebase-admin';
-import * as serviceAccount from './serviceAccount.json';
+import uploadImageToFirebase from './uploadImageToFirebase';
 
-const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url,
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(params),
-});
-
-const uploadToFirebase = async ({
-  filePath,
-  photoName,
-}: {
-  filePath: string;
-  photoName: string;
-}) => {
-  const bucketName = 'yelli-bebb3.appspot.com';
-  const dest = `upload/${photoName}`;
-  const docId = photoName.split('.')[0];
-  try {
-    await admin.storage().bucket(bucketName).upload(filePath, {
-      destination: dest,
-    });
-    await storage().bucket(bucketName).file(dest).makePublic();
-    admin
-      .firestore()
-      .collection('upload')
-      .doc(docId)
-      .set({ src: `https://storage.googleapis.com/${bucketName}/${dest}` });
-  } catch (err) {
-    throw err;
-  }
-};
-
-const uploadIpcHandler = async () => {
+const uploadIpcHandler = () => {
+  uploadImageToFirebase();
   ipcMain.handle('uploading', (_e: Event, photo: PhotoInterface) => {
     const ext = photo.src.slice(-4);
     const photoName =
@@ -63,7 +21,6 @@ const uploadIpcHandler = async () => {
       photo.thumbnail!.replace(/^data:image\/png;base64,/, ''),
       'base64'
     );
-    uploadToFirebase({ filePath, photoName });
 
     return { name: photoName, path: filePath };
   });
---
+++ src/main/ipcHandle/upload/uploadImageToFirebase.ts
@@ -0,0 +1,66 @@
+import admin, { storage } from 'firebase-admin';
+import * as serviceAccount from './serviceAccount.json';
+import { app } from 'electron';
+import chokidar from 'chokidar';
+import path from 'path';
+
+const params = {
+  type: serviceAccount.type,
+  projectId: serviceAccount.project_id,
+  privateKeyId: serviceAccount.private_key_id,
+  privateKey: serviceAccount.private_key,
+  clientEmail: serviceAccount.client_email,
+  clientId: serviceAccount.client_id,
+  authUri: serviceAccount.auth_uri,
+  tokenUri: serviceAccount.token_uri,
+  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(params),
+});
+
+const uploadImageToFirebase = () => {
+  const uploadDir = app.isPackaged
+    ? path.join(process.resourcesPath, 'assets', 'upload')
+    : path.join(__dirname, '../../../../assets', 'upload');
+
+  const uploadWatcher = chokidar.watch(uploadDir, {
+    ignored: /(^|[\/\\])\..|Icon/, // ignore dotfiles
+    depth: 0,
+    persistent: true,
+  });
+
+  uploadWatcher.on('add', (file) => {
+    const fileName = path.win32.basename(file);
+    upload({ filePath: file, photoName: fileName });
+  });
+
+  const upload = async ({
+    filePath,
+    photoName,
+  }: {
+    filePath: string;
+    photoName: string;
+  }) => {
+    const bucketName = 'yelli-bebb3.appspot.com';
+    const dest = `upload/${photoName}`;
+    const docId = photoName.split('.')[0];
+    try {
+      await admin.storage().bucket(bucketName).upload(filePath, {
+        destination: dest,
+      });
+      await storage().bucket(bucketName).file(dest).makePublic();
+      admin
+        .firestore()
+        .collection('upload')
+        .doc(docId)
+        .set({ src: `https://storage.googleapis.com/${bucketName}/${dest}` });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
+
+export default uploadImageToFirebase;
