6412b89d8e830cd00af6ec70d0467a4f596d88a1
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-04-15 18:04:01 +0700

    Modified : upload path on initialize

--- src/main/initialize.ts
+++ src/main/initialize.ts
@@ -6,13 +6,12 @@ import Store from 'electron-store';
 
 export const initialProcess = (mainWindow: BrowserWindow) => {
   const store = new Store();
-  const uploadPath = app.isPackaged
-    ? path.join(process.resourcesPath, 'assets', 'upload')
-    : path.join(__dirname, '../../assets', 'upload');
 
   let photosDir: string =
     (store.get('photosDir') as string) ||
     path.join(app.getPath('documents'), 'photos');
+
+  const uploadPath = path.join(photosDir, 'print');
 
   let isUploadDirExist = fs.existsSync(uploadPath);
 
